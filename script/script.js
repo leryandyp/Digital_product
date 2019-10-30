49717c6af4a9fda4f96b3230f357307360ad3914
leryandy <leryandy@github.com>
2019-10-30 17:10:01 -0400

    proyecto completo

---
+++ script/script.js
@@ -0,0 +1,13 @@
+var nombre = document.getElementById("nombre");
+var email = document.getElementById("email");
+var fono = document.getElementById("fono");
+
+nombre.value = "";
+email.value = "";
+fono.value = "";
+
+
+
+console.log(nombre.value);
+
+// alert("Hola");
